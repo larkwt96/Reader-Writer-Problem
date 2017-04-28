a59fea5772cdeb5e814d53a44b8820566e19e61a
Lucas Wilson <lkwilson96@gmail.com>
2017-04-27 22:22:42 -0600

    Cleaned soln.c to write to a real file, not finished yet though.

--- soln.c
+++ soln.c
@@ -1,7 +1,11 @@
 // Author: Lucas Wilson
 // Email: lkwilson96@gmail.com
 // Created for CS370 Honors Assignment
-
+/**
+ * This program provides a solution to the first reader-writer problem and will
+ * exit with a value of 1 on error, 0 otherwise. Appropriate error messages
+ * will be printed.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -14,123 +18,47 @@
 #include <sys/stat.h>
 #include <pthread.h>
 
+//******************************************************************************
+/* READER-WRITER DEFINITIONS */
 /**
- * This program provides a solution to the first reader-writer problem and will
- * exit with a value of 1 on error, 0 otherwise. Appropriate error messages
- * will be printed.
+ * The basic usage: start_thread(READER) or start_thread(WRITER). Don't call
+ * any of the other functions yourself. They are separated to demonstrate the
+ * different components to this problem.
  */
 
-/* Shared memory definitions */
-#define SHM_SIZE 1024000
-int shmid; // file descriptor (actually shared memory)
-void *shm; // shared memory pointer
-
-/* Reader writer definitions */
+/*
+ * Starts a reader or writer thread. Pass one of the READER/WRITER aliases as
+ * type to specify which.
+ */
 #define READER 1
 #define WRITER 2
-pthread_t start_thread(unsigned type); // type specifies reader/writer
-pthread_mutex_t rw; // the writers lock
-pthread_mutex_t r; // the rc lock
-int rc = 0; // count for readers, initially none
-void *reader(void*); // the reader thread routine
-void read_file(int id); // function to define how the reader reads
-void *writer(void*); // the writer thread routine
-void write_file(int id); // function to define how the writer writes
+pthread_t start_thread(unsigned type);
 
-/* Used to start and end the demonstration */
-// Called after everything is initialized. Implementation for start() is at the bottom after the test functions.
-void start();
-// Deallocates shared mem regions, etc. Exit code specified by val, but if an
-// error occurs during deallocation, val is overwritten by new error code.
-void end(int val);
+/* The locks used by the solution. See functions reader and writer. */
+pthread_mutex_t rw;         // the writers lock
+pthread_mutex_t r;          // the rc resource lock
+int rc = 0;                 // reader count
+/*
+ * The reader/writer thread routines. Takes one arg of type int* which stores
+ * the display id, used for output. Normally, I would just print the thread ID,
+ * but it was a very large number (unsigned long) and not very easy to
+ * differentiate between when dealing with 20+ total threads. The allocated
+ * int* is freed at the end of the routine.
+ */
+void *reader(void*);        // the reader thread routine
+void *writer(void*);        // the writer thread routine
+/*
+ * I wanted to separate the actual reading/writing of the file from the thread
+ * routines themselves so that these specifics could be manually implemented
+ * later, were this to be turned into a library.
+ */
+char *filename; // the file for the readers/writers to read/write to
+void read_file(int id);     // function to define how the reader reads
+#define MAX_WRITE_SIZE 1024 // the write_file writes this many random chars
+void write_file(int id);    // function to define how the writer writes
 
-/* Initialize components */
-int main() {
-    // rand() used by write_file
-    srand(time(NULL));
-    // Buffers make the output stream of multithreading inaccurate.
-    setbuf(stdout, NULL);
-
-    /* Create and attach to shared memory */
-    shmid = shmget(IPC_PRIVATE, SHM_SIZE, S_IRUSR|S_IWUSR);
-    if (shmid == -1) {
-        perror("Unable to create shared memory");
-        end(1);
-    }
-    shm = shmat(shmid, NULL, 0);
-    if (shm == (void*)-1) {
-        perror("Unable to attach to shared memory");
-        end(1);
-    } // ELSE: shmid and shm are set and valid
-
-    /* Start implementation */
-    start();
-    end(0); // clean exit :)
-}
-
-void end(int val) {
-    /* Detach from shared memory */
-    if (shmdt(shm) == -1) {
-        perror("Unable to detach from shared memory");
-        val = 1;
-    }
-    /* Delete shared memory */
-    if (shmctl(shmid, IPC_RMID, NULL) == -1) {
-        perror("Unable to delete shared memory");
-        val = 1;
-    }
-    exit(val);
-}
-
-void *reader(void *arg) {
-    int id = *((int*)arg);
-    printf("[reader: %d] Started.\n", id);
-
-    /* Implementation */
-    pthread_mutex_lock(&r);
-    if (rc++==0) pthread_mutex_lock(&rw);
-    pthread_mutex_unlock(&r);
-    read_file(id);
-    pthread_mutex_lock(&r);
-    if (--rc==0) pthread_mutex_unlock(&rw);
-    pthread_mutex_unlock(&r);
-
-    printf("[reader: %d] Ending.\n", id);
-    free(arg);
-    pthread_exit(NULL);
-}
-
-void read_file(int id) {
-    printf("[reader: %d] Started reading.\n", id);
-    int hash = 0; // calculate hash of memory
-    for (unsigned long pos = 0; pos < SHM_SIZE; pos++)
-        hash += *((char*)(shm+pos));
-    printf("[reader: %d] Reader hash: %d.\n", id, hash);
-    printf("[reader: %d] Done reading.\n", id);
-}
-
-void *writer(void *arg) {
-    int id = *((int*)arg);
-    printf("[writer: %d] Started.\n", id);
-
-    /* Implementation */
-    pthread_mutex_lock(&rw);
-    write_file(id);
-    pthread_mutex_unlock(&rw);
-
-    printf("[writer: %d] Ending.\n", id);
-    free(arg);
-    pthread_exit(NULL);
-}
-
-void write_file(int id) {
-    printf("[writer: %d] Started writing.\n", id);
-    for (unsigned long pos = 0; pos < SHM_SIZE; pos++)
-        *((char*)(shm+pos)) = rand();
-    printf("[writer: %d] Done writing.\n", id);
-}
-
-
+//******************************************************************************
+/* IMPLEMENTATION SECTION */
 pthread_t start_thread(unsigned type) {
     // counts readers/writers and assigns them a more readable id than tid
     static unsigned rc = 0;
@@ -139,11 +67,10 @@ pthread_t start_thread(unsigned type) {
     // build thread
     pthread_t tid;
     pthread_attr_t attr;
-    pthread_attr_init(&attr); // defaults are ok
+    pthread_attr_init(&attr); // defaults are fine
 
     // start thread
-    // It's weird I know. It's for readability in output.
-    int *id = malloc(sizeof(*id)); 
+    int *id = malloc(sizeof(*id));
     switch (type) {
     case READER:
         *id = ++rc;
@@ -153,12 +80,104 @@ pthread_t start_thread(unsigned type) {
         *id = ++wc;
         pthread_create(&tid, &attr, writer, id);
         break;
+    // Assume function call is valid..
     }
     return tid;
 }
 
+void *reader(void *arg) {
+    int id = *((int*)arg);
+    printf("[reader: %d] Started.\n", id);
+
+    /* IMPLEMENTATION
+     * Readers lock r, so all but one gets through, and the rest block. For the
+     * one that gets through, there are two cases. Since rc starts at zero and
+     * every increment is followed by a decrement, rc>=0
+     *
+     * Case 1: rc==0
+     * If rc is 0, there are no readers reading, so the current reader is the
+     * first. It then locks rw thus preventing writers from entering. If a
+     * writer already locked it, the reader will block here until the writer is
+     * done. While it's blocked any other readers are still blocked on r above
+     * ensuring that no other readers enter while the writer is writing. When
+     * the lock is released and the first reader is able to lock rw, it unlocks
+     * r and starts reading. Any immediate subsequent readers fall into case 2
+     * now that rc has been incremented.
+     *
+     * Case 2: rc>0
+     * If rc is non zero, the readers are able to lock r, increment rc, and
+     * unlock r for the next reader. They don't attempt to lock rw since rc==0
+     * is false and therefore will never block.
+     */
+    pthread_mutex_lock(&r);
+    if (rc++==0) pthread_mutex_lock(&rw);
+    pthread_mutex_unlock(&r);
+
+    read_file(id);
+
+    /*
+     * Since readers are reading, rw is locked, so no writers are writing. We
+     * can therefore ignore them since they are all blocked. There are then two
+     * cases of interest. At this point, rc has been incremented at least once,
+     * so rc>=1
+     *
+     * Case 1: rc==1
+     * If rc==1, then it's the only reader reading. Since r is locked, no
+     * readers can enter. rc is decremented before boolean evaluation so the if
+     * statement is true and rw is unlocked. 
+     *
+     * Case 2: rc>1
+     * If rc is non zero, rc is decremented. Done enough times, case 1 is
+     * induced.
+     */
+    pthread_mutex_lock(&r);
+    if (--rc==0) pthread_mutex_unlock(&rw);
+    pthread_mutex_unlock(&r);
+
+    printf("[reader: %d] Ending.\n", id);
+    free(arg);
+    pthread_exit(NULL);
+}
+
+void *writer(void *arg) {
+    int id = *((int*)arg);
+    printf("[writer: %d] Started.\n", id);
+
+    /* Implementation */
+    /*
+     * First writer locks rw. If it is already locked because of a reader,
+     * writer blocks until a reader unlocks it. All subsequent writers block
+     * here until rw is unlocked.
+     */
+    pthread_mutex_lock(&rw);
+    write_file(id);
+    /*
+     * Unlocks rw signaling that writing is complete, at which point a reader
+     * or writer (either when one shows up later or if one is already waiting)
+     * will lock rw and begin reading/writing.
+     */
+    pthread_mutex_unlock(&rw);
+
+    printf("[writer: %d] Ending.\n", id);
+    free(arg);
+    pthread_exit(NULL);
+}
+
+void read_file(int id) {
+    printf("[reader: %d] Started reading.\n", id);
+    // READ file
+    printf("[reader: %d] Done reading.\n", id);
+}
+
+void write_file(int id) {
+    printf("[writer: %d] Started writing.\n", id);
+    // WRITE to file
+    printf("[writer: %d] Done writing.\n", id);
+}
+
+//******************************************************************************
 /* TEST FUNCTIONS */
-void easy_test() {
+void simple_test() {
     printf("Starting reader thread...\n");
     pthread_t rtid = start_thread(READER);
     printf("Started reader thread.\n");
@@ -176,7 +195,7 @@ void easy_test() {
     printf("Joined writer thread.\n");
 }
 
-void x_of_each(unsigned count) {
+void xofeach(unsigned count) {
     printf("Starting a total of %u readers and writers.\n", count*2);
     pthread_t threads[count*2];
     for (unsigned i = 0; i < count; i++) {
@@ -199,13 +218,27 @@ void starve_writer() {
     for (int i = 11; i < 1001; i++) threads[i] = start_thread(READER);
     for (unsigned i = 0; i < 1001; i++) pthread_join(threads[i], NULL);
 }
-/* END OF TEST FUNCTIONS */
-/**
- * This is where to customize how the implementation is tested. The easiest way
- * is to create a different function for different test cases and just call
- * that function from here.
- */
-void start() {
-    //starve_writer();
-    x_of_each(10);
+
+//******************************************************************************
+/* MAIN */
+int main(int argc, char *argv[]) {
+    // rand() used by write_file
+    srand(time(NULL));
+    // Buffers make the output stream of multithreading inaccurate.
+    setbuf(stdout, NULL);
+
+    /* Check args */
+    if (argc != 2) { // Incorrect number of arguments
+        fprintf(stderr, "Usage: %s file\n", argv[0]);
+        return 1;
+    } else { // File doesn't exist
+        filename = argv[1];
+        if (access(filename, R_OK|W_OK) == -1) {
+            perror("Please provide existing file with rw permissions");
+            return 1;
+        }
+    }
+
+    /* Start implementation, call test function */
+    xofeach(10);
 }
